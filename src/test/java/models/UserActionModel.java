395a1ea4f4a0ed63f5d00cd2acdc982398d9137c
LSKozarina <lubov.kozarina@pix.ru>
2025-05-02 22:57:39 +0300

    removed models

--- src/test/java/models/UserActionModel.java
+++ src/test/java/models/UserActionModel.java
@@ -1,7 +1,9 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class UserActionModel {
     private String name;
